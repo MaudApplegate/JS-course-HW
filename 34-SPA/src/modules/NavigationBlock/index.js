05113e5c3ed89f9203d26804d47b90228874e5e8
Varvara Brel <MaudApplegate@gmail.com>
2022-02-02 10:34:12 +0200

    deleted recovery

---
+++ 34-SPA/src/modules/NavigationBlock/index.js
@@ -0,0 +1,6 @@
+import AvrilBlock from '../AvrilBlock/index.js';
+import PizzaBlock from '../PizzaBlock/index.js';
+import UserDataBlock from '../UserDataBlock/index.js';
+import './style.scss';
+
+console.log('Nav Block');
