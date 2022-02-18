6218bbaf7b06dfb1053bf808197c4e4cbe6a877a
Varvara Brel <MaudApplegate@gmail.com>
2022-02-18 18:17:15 +0200

    all is done

--- 41-React/src/App.jsx
+++ 41-React/src/App.jsx
@@ -1,11 +1,30 @@
+import DataBlock from './components/DataBlock';
 import Form from './components/Form';
-import Test from './components/Form/test';
+import FormRenderProps from './components/FormRenderProps';
+import Registration from './components/FormRenderProps/registration';
+import Entry from './components/FormRenderProps/entry';
+import DeleteAcc from './components/FormRenderProps/deleteAcc';
 
 function App() {
   return (
     <div>
-      <Test />
-      <Form />
+      {/* <Form /> */}
+      {/* <DataBlock /> */}
+      <FormRenderProps
+        render={() => {
+          return <Registration />;
+        }}
+      />
+      <FormRenderProps
+        render={() => {
+          return <Entry />;
+        }}
+      />
+      <FormRenderProps
+        render={() => {
+          return <DeleteAcc />;
+        }}
+      />
     </div>
   );
 }
